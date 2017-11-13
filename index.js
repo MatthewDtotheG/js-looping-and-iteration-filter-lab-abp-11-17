c0ac82a6fdf38fb5457572736f3236227953e2f3
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-13 01:57:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ function findMatching(list, name) {
     return driverName.toLowerCase() === name.toLowerCase();
   });
 }
-function fuzzyMatch (list, partialName) {
+function fuzzyMatch(list, partialName) {
   let lengthOfName = partialName.length;
-  return list.filter(function (driverName) {
+  return list.filter(function(driverName) {
     return driverName.slice(0, lengthOfName) === partialName;
   });
 }
-
-function matchName (list, name) {
-  return list.filter(function (driver) {
+function matchName(list, name) {
+  return list.filter(function(driver) {
     return driver.name.toLowerCase() === name.toLowerCase();
   });
 }
